15b1a46c2906d6cb9f6b2531d4d7994f36a59125
yofanurman26 <yofanurman26@gmail.com>
2022-06-15 21:51:29 +0700

    add table

--- src/components/RiwayatGempa.tsx
+++ src/components/RiwayatGempa.tsx
@@ -1,5 +1,46 @@
 const RiwayatGempa = () => {
-	return <div>RiwayatGempa</div>;
+	return <div><table>
+	<tr>
+	<th>Lokasi</th>
+	  <th>Magnitudo</th>
+	  <th>Kedalaman</th>
+	  <th>Waktu</th>
+	  <th>Koordinat</th>
+	  <th>Keterangan</th>
+	</tr>
+	<tr>
+	<td>17 km Tenggara SARMI-PAPUA</td>
+	  <td>5,0</td>
+	  <td>10 km</td>
+	  <td>10 jam yang lalu</td>
+	  <td>-2.53,139.35</td>
+	  <td>Tidak berpotensi tsunami</td>
+	</tr>
+	<tr>
+	<td>17 km Tenggara SARMI-PAPUA</td>
+	  <td>5,0</td>
+	  <td>10 km</td>
+	  <td>10 jam yang lalu</td>
+	  <td>-2.53,139.35</td>
+	  <td>Tidak berpotensi tsunami</td>
+	</tr>
+	<tr>
+	<td>17 km Tenggara SARMI-PAPUA</td>
+	  <td>5,0</td>
+	  <td>10 km</td>
+	  <td>10 jam yang lalu</td>
+	  <td>-2.53,139.35</td>
+	  <td>Tidak berpotensi tsunami</td>
+	</tr>
+	<tr>
+	<td>17 km Tenggara SARMI-PAPUA</td>
+	  <td>5,0</td>
+	  <td>10 km</td>
+	  <td>10 jam yang lalu</td>
+	  <td>-2.53,139.35</td>
+	  <td>Tidak berpotensi tsunami</td>
+	</tr>
+  </table></div>;
 };
 
 export default RiwayatGempa;
