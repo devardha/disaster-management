805922875c2652e9c5cb15c5b4ca0971ca34cd71
devardha <ardhaydhtm@gmail.com>
2022-06-15 20:32:17 +0700

    Install embla carousel and downgrade react to 17.0.2

--- src/components/PrakiraanCuaca/index.tsx
+++ src/components/PrakiraanCuaca/index.tsx
@@ -1,7 +1,13 @@
 import style from "./PrakiraanCuaca.module.css";
 
 const PrakiraanCuaca = () => {
-	return <div className="container">Prakiraan Cuaca</div>;
+	return (
+		<div className="container">
+			<h2>Prakiraan Cuaca</h2>
+			<h3>Jakarta, Indonesia</h3>
+			<div></div>
+		</div>
+	);
 };
 
 export default PrakiraanCuaca;
