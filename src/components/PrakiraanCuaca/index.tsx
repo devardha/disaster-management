485ad6fc130f0ea0c6d9ab977d9dfbe76e729ce1
devardha <ardhaydhtm@gmail.com>
2022-06-16 20:55:08 +0700

    Add carousel to prakiraan cuaca section

--- src/components/PrakiraanCuaca/index.tsx
+++ src/components/PrakiraanCuaca/index.tsx
@@ -1,12 +1,51 @@
-import style from "./PrakiraanCuaca.module.css";
+import Carousel from "../Carousel";
+
+const data: any = [
+	{
+		code: "0",
+	},
+	{
+		code: "0",
+	},
+	{
+		code: "0",
+	},
+	{
+		code: "0",
+	},
+	{
+		code: "0",
+	},
+	{
+		code: "0",
+	},
+];
 
 const PrakiraanCuaca = () => {
 	return (
-		<div className="container">
-			<h2>Prakiraan Cuaca</h2>
-			<h3>Jakarta, Indonesia</h3>
-			<div></div>
-		</div>
+		<>
+			<div className="container">
+				<h2>Prakiraan Cuaca</h2>
+				<h3>Jakarta, Indonesia</h3>
+				<Carousel data={data} />
+			</div>
+			<style>{`
+				.container{
+					padding-top: 52px;
+					padding-bottom: 52px;
+				}
+
+				h2{
+					margin:0;
+					margin-bottom:4px;
+				}
+				h3{
+					margin:0;
+					margin-bottom:20px;
+					text-align:left;
+				}
+			`}</style>
+		</>
 	);
 };
 
