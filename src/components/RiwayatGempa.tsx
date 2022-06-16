c52cbe230b44a35a33b16b1c284edcf932fc97e5
yofanurman26 <yofanurman26@gmail.com>
2022-06-16 21:59:56 +0700

    Add style to table

--- src/components/RiwayatGempa.tsx
+++ src/components/RiwayatGempa.tsx
@@ -1,5 +1,6 @@
 const RiwayatGempa = () => {
 	return <div><table>
+		<thead>
 	<tr>
 	<th>Lokasi</th>
 	  <th>Magnitudo</th>
@@ -8,6 +9,8 @@ const RiwayatGempa = () => {
 	  <th>Koordinat</th>
 	  <th>Keterangan</th>
 	</tr>
+	</thead>
+	<tbody>
 	<tr>
 	<td>17 km Tenggara SARMI-PAPUA</td>
 	  <td>5,0</td>
@@ -40,7 +43,16 @@ const RiwayatGempa = () => {
 	  <td>-2.53,139.35</td>
 	  <td>Tidak berpotensi tsunami</td>
 	</tr>
-  </table></div>;
+	</tbody>
+  </table>
+  <style>{`
+  thead{
+	background-color:#eeeeee;
+  }
+  
+  `}
+</style>
+  </div>;
 };
 
 export default RiwayatGempa;
