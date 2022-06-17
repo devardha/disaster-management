ede7ce812c45cfc75b75d266341ada306efdb0fd
devardha <ardhaydhtm@gmail.com>
2022-06-17 20:03:48 +0700

    Fix navbar link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 const Navbar = () => {
@@ -9,13 +10,13 @@ const Navbar = () => {
 				</div>
 				<ul>
 					<li>
-						<a href="../PrakiraanCuaca/index.tsx">Prakiraan Cuaca</a>
+						<Link to="/">Prakiraan Cuaca</Link>
 					</li>
 					<li>
-						<a href="../LaporanTerkini/LaporanTerkini.tsx">Berita Terkini</a>
+						<Link to="/">Berita Terkini</Link>
 					</li>
 					<li>
-						<a href="../Lapor/formLapor.jsx">Lapor</a>
+						<Link to="/lapor">Lapor</Link>
 					</li>
 				</ul>
 			</div>
