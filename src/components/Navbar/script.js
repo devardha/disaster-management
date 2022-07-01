14ca78086eac6fed98257ad5223a64935c708756
devardha <ardhaydhtm@gmail.com>
2022-07-01 17:49:43 +0700

    Fix file structure

--- src/components/Navbar/script.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector(".menu input");
-const nav = document.querySelector("ul");
-
-menu.addEventListener('click', function(){
-nav.classList.toggle('slide')
-});	
-
-export default menu
