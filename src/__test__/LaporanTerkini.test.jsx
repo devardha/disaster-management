88e284ac0a329afbd712b924f01d89bd57b1e267
Fahmi Jamaludin <fahmijamaludin2@gmail.com>
2022-07-03 22:02:05 +0700

    menambahkan testing

--- src/__test__/LaporanTerkini.test.jsx
+++
@@ -1,7 +0,0 @@
-import userEvent from '@testing-library/user-event';
-import {render, screen} from '@testing-library/react';
-import LaporanTerkini from '../components/LaporanTerkini/LaporanTerkini'
-
-test('', () =>{
-    
-});
